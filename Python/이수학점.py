d2e9c93cd41e6e98b7179c7a7dd161c0f99f41eb
SEOULminusL <leesw0901@duksung.ac.kr>
2022-11-14 12:28:39 +0900

    2022학번 이수학점 제외

--- Python/이수학점.py
+++ Python/이수학점.py
@@ -7,20 +7,18 @@ Created on Mon Oct 31 13:28:04 2022
 import pandas as pd
 import numpy as np
 
-data1 = pd.read_excel('성적테이블.xlsx',  header = 0, usecols = [0,4])
+data1 = pd.read_excel('성적테이블.xlsx',  header = 0, usecols = [0,1,4])
 
-data1['이수학점'] = 0
+idx_nm_22 = data1[data1['학년도']== 2022 ].index
 
-data1.rename(columns = {"취득학점(평균)" : "Credit" }, inplace=True)
+data2 = data1.drop(idx_nm_22)
 
-data1.head()
-data1.info()
+data3 = data2.groupby('ID').sum()
+#data1['이수학점'] = 0
 
-#data1 = data1.drop(['학년도', '학기' ,'평점(평균)'], axis=1)
+data3 = data3.drop(columns = "학년도" ,  axis=1)
 
-data2 = data1.groupby('ID').sum()
-
-
-data2.to_csv('이수학점.csv',index = True)
+data3.rename(columns = {"취득학점(평균)" : "취득학점(총합)" }, inplace=True)
 
 
+data3.to_csv('총합이수학점_2022제외.csv',index = True)
